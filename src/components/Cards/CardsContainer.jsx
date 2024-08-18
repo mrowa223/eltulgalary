be47a50e86552a8da4a2e17179780731a0a7ca68
Kirill <kirillshermadov.cs@gmail.com>
2024-08-18 23:22:02 +0500

    core: add grid for cards container

--- src/components/Cards/CardsContainer.jsx
+++ src/components/Cards/CardsContainer.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import Profile from "./Profile/Profile";
-
 import cardsData from "../../data/cardsData";
 import "./CardsContainer.scss"
 
 const CardsContainer = () => {
-
-
   return (
-    <div className="grid grid-cols-4 gap-4 justify-center cards-container">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center cards-container">
       {cardsData.map((card, index) => (
         <Profile
           key={index}
           image={card.image}
           name={card.name}
           title={card.title}
-          circleImage ={card.circleImage}
+          circleImage={card.circleImage}
           // description={card.description}
         />
       ))}
